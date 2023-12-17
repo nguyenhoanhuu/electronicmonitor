52b4281bb395d0de15b4f53b64783a9601b54e7d
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2023-12-17 22:42:18 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Login from "./pages/Login";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
